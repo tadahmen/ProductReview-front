440fb1d392e4c5aa8b464ac7101bd74cbc0d2cda
tadahmen <tadahmen@gmail.com>
2016-03-11 15:01:36 +0100

    prepared config files for deploy

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -25,10 +25,18 @@ module.exports = {
     })
   ],
   module: {                   //same
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'src')
-    }]
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src')
+      },
+      { test: /\.woff2?$/,   loader: "url-loader?limit=10000&minetype=application/font-woff" },
+      { test: /\.ttf$/,    loader: "file-loader" },
+      { test: /\.eot$/,    loader: "file-loader" },
+      { test: /\.svg$/,    loader: "file-loader" },
+      { test: /\.png$/,    loader: "file-loader" },
+      { test: /\.scss$/,   loader: 'style!css!sass'}
+    ]
   }
 };
